444653873b6f4387a2a182172a8d190af03d172e
Jaroslav Pesek <pesekja8@fit.cvut.cz>
2025-03-19 16:55:26 +0100

    feat(decrypto): add python env

--- pkgs/decrypto/default.nix
+++ pkgs/decrypto/default.nix
@@ -8,11 +8,22 @@
   cacert,
   libdst,
   python3,
-  boost
+  boost,
+  fetchPypi
 }:
 
 let
-  pythonEnv = python3.withPackages (ps: [ ps.numpy ]);
+  nemea-pytrap = python3.pkgs.buildPythonPackage rec {
+    pname = "nemea-pytrap";
+    version = "0.17.0";
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-Bpst1oU9ZbODgpF0oayjfT20wiheoG3HiLRWYyaqPa8=";
+    };
+    propagatedBuildInputs = with python3.pkgs; [ setuptools nemea-framework ];
+  };
+
+  pythonEnv = python3.withPackages (ps: [ nemea-pytrap ps.numpy ps.xxhash ]);
 in
 stdenv.mkDerivation rec {
   pname = "decrypto";
@@ -27,6 +38,12 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ cmake git cacert ];
   buildInputs = [ wif nemea-framework libdst pythonEnv boost ];
+
+  postInstall = ''
+    tar -xvf $src -C $TMPDIR
+    cp -r $TMPDIR/decrypto-main/aggregator $out/bin/
+    ln -s ${pythonEnv} $out/bin/python-decrypto
+  '';
 
   meta = {
     description = "NEMEA module for detection of cryptomining, based on libnemea++ and libwif.";
